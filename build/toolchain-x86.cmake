dd3d3ace8513b86cee21fa668f87902913dd672c
Koen Bijttebier <koen.bijttebier@telenet.be>
2021-09-04 23:20:51 +0200

    force gcc 4.8

--- build/toolchain-x86.cmake
+++ build/toolchain-x86.cmake
@@ -1,4 +1,7 @@
 
 set( UBIE_APP_PLATFORM "x86")
 
+#Force Gcc (setting alternatives does not seem to work)
+SET(CMAKE_C_COMPILER /usr/bin/x86_64-linux-gnu-gcc-4.8)
+SET(CMAKE_CXX_COMPILER /usr/bin/x86_64-linux-gnu-g++-4.8)
 
